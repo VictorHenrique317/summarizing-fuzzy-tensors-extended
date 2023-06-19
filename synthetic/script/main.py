372d75cd141ef40402e0ef1ad3a890437b40b61d
Victor Henrique <victor.henrique5800@gmail.com>
2023-06-19 14:23:03 +0000

    adding Dockerfile

--- synthetic/script/main.py
+++ synthetic/script/main.py
@@ -15,5 +15,5 @@ triclusterbox = TriBiclusterBox(controller)
 nclusterboxnoperformanceimp = NclusterBoxNoPerformanceImp(controller)
 # ========================== ALGORITHMS USED ========================== #
 
-# controller.initiateSession()
-controller.initiatePostAnalysis()
+controller.initiateSession()
+#controller.initiatePostAnalysis()
