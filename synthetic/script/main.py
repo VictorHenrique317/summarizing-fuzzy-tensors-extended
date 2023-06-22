ec5fd5bce19a09f8daae445f338f6972be539284
Victor Henrique <victor.henrique5800@gmail.com>
2023-06-22 08:10:22 -0300

    Sync

--- synthetic/script/main.py
+++ synthetic/script/main.py
@@ -8,11 +8,11 @@ from algorithm.nclusterbox_no_performance_imp import NclusterBoxNoPerformanceImp
 controller = Controller()
 
 # ========================== ALGORITHMS USED ========================== #
-getf = Getf(controller)
-cancer = Cancer(controller)
+# getf = Getf(controller)
+# cancer = Cancer(controller)
 nclusterbox = NclusterBox(controller)
-triclusterbox = TriBiclusterBox(controller)
-nclusterboxnoperformanceimp = NclusterBoxNoPerformanceImp(controller)
+# triclusterbox = TriBiclusterBox(controller)
+# nclusterboxnoperformanceimp = NclusterBoxNoPerformanceImp(controller)
 # ========================== ALGORITHMS USED ========================== #
 
 controller.initiateSession()
