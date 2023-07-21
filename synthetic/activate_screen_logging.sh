8194854ba9018794481b1a7bdda8b4cd74359b35
Victor Henrique <victor.henrique5800@gmail.com>
2023-07-21 17:06:15 +0000

    Fixed tribiclusterbox and added selection time

--- synthetic/activate_screen_logging.sh
+++ synthetic/activate_screen_logging.sh
@@ -1,5 +1,2 @@
-log_filename="screen_logs/$(date +'%d_%m_%Y').txt"
-session_name=$(screen -ls | grep -oP '(?<=\d\.)\S+')
-
-screen -r $session_name -X logfile screen_logs/$filename
-screen -r $session_name -X log
+screen -r pts-0.gorgona5 -X logfile screen "screen_logs/$(date +'%d_%m_%Y').txt"
+screen -r pts-0.gorgona5  -X log
